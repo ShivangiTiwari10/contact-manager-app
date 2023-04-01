6d787f65978332080c3a9eaf00c08093d8cbe5e9
Shivangi <shivangit1111@gmail.com>
2023-04-01 17:09:33 +0530

    feat: Bind View

--- app/src/main/java/com/example/contactmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/contactmanagerapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.contactmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.contactmanagerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
