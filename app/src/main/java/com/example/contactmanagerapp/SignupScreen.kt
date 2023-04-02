99437e6989f975f16ad2655414651279e77a1e5b
Shivangi <shivangit1111@gmail.com>
2023-04-02 11:29:00 +0530

    feat: bind View

--- app/src/main/java/com/example/contactmanagerapp/SignupScreen.kt
+++ app/src/main/java/com/example/contactmanagerapp/SignupScreen.kt
@@ -2,10 +2,14 @@ package com.example.contactmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.contactmanagerapp.databinding.ActivitySignupScreenBinding
 
 class SignupScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_screen)
+
+        binding = ActivitySignupScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
