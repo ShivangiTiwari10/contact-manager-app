067a5539103e3fe7586d1b608e600366ce5527bf
Shivangi <shivangit1111@gmail.com>
2023-04-03 16:02:50 +0530

    feat: bind view

--- app/src/main/java/com/example/contactmanagerapp/AddContacts.kt
+++ app/src/main/java/com/example/contactmanagerapp/AddContacts.kt
@@ -2,10 +2,14 @@ package com.example.contactmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.contactmanagerapp.databinding.ActivityAddContectsBinding
 
 class AddContacts : AppCompatActivity() {
+    private lateinit var binding: ActivityAddContectsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_contects)
+
+        binding = ActivityAddContectsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
