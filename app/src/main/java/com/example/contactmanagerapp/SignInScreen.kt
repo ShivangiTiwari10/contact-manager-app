5343f0784cb6396fd37606d0edeb9e96daa71c69
Shivangi <shivangit1111@gmail.com>
2023-04-03 15:36:18 +0530

    feat: logged in user

--- app/src/main/java/com/example/contactmanagerapp/SignInScreen.kt
+++ app/src/main/java/com/example/contactmanagerapp/SignInScreen.kt
@@ -2,15 +2,48 @@ package com.example.contactmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.contactmanagerapp.databinding.ActivitySignInScreenBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignInScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInScreenBinding
+
+    private lateinit var database: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignInScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val userPassword = binding.edPassword.text.toString()
+            if (userPassword.isNotEmpty()) {
+                getData(userPassword)
+            } else {
+                Toast.makeText(this, "Please enter user id", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getData(userPassword: String) {
+        database = FirebaseDatabase.getInstance().getReference("Users")
+
+        database.child(userPassword).get().addOnSuccessListener {
+
+
+            if (it.exists()) {
+
+                Toast.makeText(this, "User exists", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
