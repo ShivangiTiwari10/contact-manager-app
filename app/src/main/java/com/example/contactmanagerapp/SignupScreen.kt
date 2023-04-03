a4f85d05f5818220810fb461c237d0e738c013a9
Shivangi <shivangit1111@gmail.com>
2023-04-03 13:39:33 +0530

    feat: initialize database reference

--- app/src/main/java/com/example/contactmanagerapp/SignupScreen.kt
+++ app/src/main/java/com/example/contactmanagerapp/SignupScreen.kt
@@ -3,13 +3,18 @@ package com.example.contactmanagerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.contactmanagerapp.databinding.ActivitySignupScreenBinding
+import com.google.firebase.database.DatabaseReference
 
 class SignupScreen : AppCompatActivity() {
     private lateinit var binding: ActivitySignupScreenBinding
+
+    lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         binding = ActivitySignupScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
